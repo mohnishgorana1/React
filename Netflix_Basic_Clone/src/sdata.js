ead8341504543a2cb4ed83cae8a568157ff2301e
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-07-30 20:12:58 +0530

    deleted eslint
    no use her

---
+++ Netflix_Basic_Clone/src/sdata.js
@@ -0,0 +1,34 @@
+const sdata = [
+    {
+        sname: "DARK",
+        imgscr: "https://w0.peakpx.com/wallpaper/592/913/HD-wallpaper-jonas-dark-dark-netflix-dark-series-everything-is-connected-netflix-triquetra-thumbnail.jpg",
+        title: "NETFLIX ORIGINAL SERIES",
+        links: "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        sname: "EXTRA CURRICULAR",
+        imgscr: "https://th.bing.com/th/id/OIP.OxBzQXfklGSHxPgTH7NQ9AHaK-?pid=ImgDet&rs=1",
+        title: "NETFLIX ORIGINAL SERIES",
+        links: "https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        sname: "STRANGER THINGS",
+        imgscr: "https://wallpapercave.com/wp/wp1917154.jpg",
+        title: "NETFLIX ORIGINAL SERIES",
+        links: "https://www.netflix.com/in/title/80057281"
+    },
+    {
+        sname: "THE VAMPIRE DIARIES",
+        imgscr: "https://th.bing.com/th/id/OIP.5HPo7lP7SwRmguJ7pVMeqQAAAA?pid=ImgDet&rs=1",
+        title: "NETFLIX ORIGINAL SERIES",
+        links: "https://www.netflix.com/in/title/70143860"
+    },
+    {
+        sname: "MY FIRST-2 LOVE",
+        imgscr: "https://flxt.tmsimg.com/assets/p17080748_b_h9_aa.jpg",
+        title: "NETFLIX ORIGINAL SERIES",
+        links: "https://www.netflix.com/in/title/81026700"
+    },
+]
+
+export default sdata;
